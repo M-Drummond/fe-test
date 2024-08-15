5e2c2b5e06d2824564803b2f58cabc01b56a738c
Mackinley <mack.drummond@gmail.com>
2024-08-15 11:59:02 +1000

    pinia components

---
+++ vite-project/src/components/CountButton.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { useCounterStore } from  '../stores/PiniaStore.ts'
+const cs = useCounterStore()
+</script>
+
+<template>
+    <buttton class="p-4 bg-green-100"@click="cs.increment()">Add 1</buttton>
+</template>
---
+++ vite-project/src/components/CountDisplay.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { useCounterStore } from  '../stores/PiniaStore.ts'
+const cs = useCounterStore()
+</script>
+
+
+<template>
+    <div class="text-3xl">
+        {{ cs.count }}
+    </div>
+</template>
