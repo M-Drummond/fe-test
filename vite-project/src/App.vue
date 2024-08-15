4045370a18aba72556e2df1c4d5101adeb3c2d5e
Mackinley <mack.drummond@gmail.com>
2024-08-15 11:58:50 +1000

    add pinia imports

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,23 +1,21 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import HeroSection from './components/HeroSection.vue'
-import SiteFooter from './components/SiteFooter.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import HeroSection from './components/HeroSection.vue'
+// import SiteFooter from './components/SiteFooter.vue'
+
+import CountDisplay from './components/CountDisplay.vue'
+
+import CountButton from './components/CountButton.vue'
+
 </script>
 
 <template>
-  <div class="flex flex-row items-center justify-center space-x-4 bg-slate-400 py-4 px-8 shadow-lg sticky left-0 right-0 top-0 mb-4">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-    </div>
-  <HeroSection />
-  <HelloWorld msg="Vite + Vue" />
 
-  <SiteFooter msg="fin" />
+  <div class="w-full flex flex-row items-center space-x-4 mx-auto text-center max-w-sm bg-purple-300">
+
+    <CountButton />
+    <CountDisplay />
+
+  </div>
 
 </template>
-
- 
